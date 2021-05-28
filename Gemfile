a18ed6b50c40594e07aafd2281b58b81fdf69953
julianpetriv <petriv.j@gmail.com>
2021-05-28 06:01:43 +0300

    working image upload to DO Spaces

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'simple_command'
 
 gem 'http'
 
+gem 'aws-sdk-s3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@
 
 # Working with articles
 class ArticlesController < ApplicationController
-  skip_before_action :authenticate_request, except: [:create, :update, :destroy]
+  skip_before_action :authenticate_request, except: %i[create update destroy]
   # GET /articles
   def index
     render json: Article.all.select(:id, :title).order('id ASC')
@@ -15,7 +15,9 @@ class ArticlesController < ApplicationController
 
   # POST /articles
   def create
-    article = Article.new(article_params)
+    data = article_params
+    data = article_params.merge({ image: upload_image }) unless params[:image].nil?
+    article = Article.new(data)
     if article.save
       render json: article
     else
@@ -26,7 +28,10 @@ class ArticlesController < ApplicationController
   # PUT /articles/:id
   def update
     article = Article.find(params[:id])
-    if article.update(article_params)
+    delete_image(article.image) unless article.image.nil?
+    data = article_params
+    data = article_params.merge({ image: upload_image }) unless params[:image].nil?
+    if article.update(data)
       render json: article
     else
       render json: { errors: article.errors.full_messages }, status: 400
@@ -36,6 +41,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/:id
   def destroy
     article = Article.find(params[:id])
+    delete_image(article.image) unless article.image.nil?
     article.destroy
     render json: { message: 'Article destroyed successfully' }
   end
@@ -44,5 +50,37 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body, :status)
+  end
+
+  require 'aws-sdk-s3'
+  require 'base64'
+  def upload_image
+    path = 'images/' + ('a'..'z').to_a.sample(8).join + '.jpg'
+    file = StringIO.new(Base64.decode64(params[:image].split(',')[1]))
+    client = aws_client
+    client.put_object({
+      bucket: 'rails-blog',
+      key: path,
+      body: file,
+      acl: 'public-read'
+    })
+    path
+  end
+
+  def delete_image(path)
+    client = aws_client
+    client.delete_object({
+      bucket: 'rails-blog',
+      key: path
+    })
+  end
+
+  def aws_client
+    Aws::S3::Client.new(
+      access_key_id: ENV['SPACES_KEY'],
+      secret_access_key: ENV['SPACES_SECRET'],
+      endpoint: 'https://fra1.digitaloceanspaces.com',
+      region: 'us-east-1'
+    )
   end
 end
